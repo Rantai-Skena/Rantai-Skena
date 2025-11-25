483b6bfba1c952c61022685f24907d1fb04ff61d
HeraldoArman <heraldoarman7@gmail.com>
2025-11-26 01:14:38 +0700

    fix: resolve merge conflict from remote

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -97,7 +97,7 @@ export const HeroHeader = () => {
           className={cn(
             "mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12",
             isScrolled &&
-            "max-w-4xl rounded-2xl border bg-background/60 shadow-md backdrop-blur-lg lg:px-5",
+              "max-w-4xl rounded-2xl border bg-background/60 shadow-md backdrop-blur-lg lg:px-5",
           )}
         >
           <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
@@ -128,7 +128,7 @@ export const HeroHeader = () => {
                       className={cn(
                         "block text-muted-foreground underline-offset-4 duration-150 hover:text-accent-foreground hover:underline",
                         pathname === item.href &&
-                        "text-white hover:text-white hover:no-underline",
+                          "text-white hover:text-white hover:no-underline",
                       )}
                       href={item.href as unknown as UrlObject}
                     >
@@ -148,7 +148,7 @@ export const HeroHeader = () => {
                         className={cn(
                           "block text-muted-foreground underline-offset-4 duration-150 hover:text-accent-foreground hover:underline",
                           pathname === item.href &&
-                          "text-white hover:text-white hover:no-underline",
+                            "text-white hover:text-white hover:no-underline",
                         )}
                         href={item.href as unknown as UrlObject}
                       >
